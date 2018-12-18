1db0378dc1dc9ec0eb12ef1c0033117ca147f7b6
batbileg <bileedmn@gmail.com>
2018-12-18 00:24:00 -0600

    Bug fix

--- src/main/java/edu/mum/entity/AirLine.java
+++ src/main/java/edu/mum/entity/AirLine.java
@@ -1,9 +1,12 @@
 package edu.mum.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+
+@Entity
 public class AirLine {
 
     @Id
